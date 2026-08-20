0362a7ddb6c2b74b33afcb899685ed0f565b1dc7
Daniel Velkov <danielvelkov13@gmail.com>
2026-08-20 18:27:35 +0300

    feat: add background image that will be searched

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,9 +1,24 @@
 // import { useState } from 'react';
-import "./App.css";
+import Modal from '../components/util/Modal';
+import './App.css';
+import CabinetImg from '../assets/images/thought_cabinet.webp';
 // import styled from 'styled-components';
 
 function App() {
-  return <></>;
+  // const [isGameSessionActive, setIsGameSessionActive] = useState(false);
+  // const [isModalOpen, setIsModalOpen] = useState(true);
+  return (
+    <>
+      <Modal>
+        <h1>Hello world</h1>
+      </Modal>
+      <header></header>
+      <main>
+        <img src={CabinetImg}></img>
+      </main>
+      <footer></footer>
+    </>
+  );
 }
 
 export default App;
