23508c24a4f6308689a2ab40956601a5b682a69a
Daniel Velkov <danielvelkov13@gmail.com>
2026-08-20 18:26:13 +0300

    feat: add modal util component

---
+++ src/components/util/Modal.jsx
@@ -0,0 +1,70 @@
+import PropTypes from 'prop-types';
+import { useRef } from 'react';
+import styled from 'styled-components';
+
+const StyledDialog = styled.dialog`
+  padding: 0;
+  margin: 0;
+  inset: 0;
+  position: fixed;
+  width: 100%;
+  height: 100%;
+  background: transparent;
+  display: grid;
+  place-items: center;
+  z-index: 99;
+
+  &::backdrop {
+    display: none;
+  }
+`;
+
+const Backdrop = styled.div`
+  position: fixed;
+  inset: 0;
+  background: rgba(0, 0, 0, 0.75);
+`;
+
+const ModalWrapper = styled.div`
+  position: relative;
+  z-index: 10;
+  display: grid;
+  place-content: center;
+`;
+
+const ModalContent = styled.div`
+  position: relative;
+  width: 100%;
+  padding: 1.5rem;
+  border: 2px solid ${(props) => props.theme.border.primary};
+  border-radius: 0.75rem;
+  box-shadow: 0 10px 30px rgba(0, 0, 0, 0.2);
+  background-color: ${(props) => props.theme.background.elevated};
+`;
+
+export function Modal({ children, ...rest }) {
+  const dialogRef = useRef(null);
+
+  const handleBackdropClick = (e) => {
+    if (e.target === e.currentTarget) e.stopPropagation();
+  };
+
+  return (
+    <StyledDialog ref={dialogRef} {...rest}>
+      <Backdrop onClick={handleBackdropClick} />
+
+      <ModalWrapper>
+        <ModalContent onClick={(e) => e.stopPropagation()}>
+          {children}
+        </ModalContent>
+      </ModalWrapper>
+    </StyledDialog>
+  );
+}
+
+Modal.propTypes = {
+  children: PropTypes.arrayOf(PropTypes.node),
+  onClose: PropTypes.func,
+};
+
+export default Modal;
