5584d772e4d2a05d94b58ef63fd5014929a6b2af
Daniel Velkov <danielvelkov13@gmail.com>
2026-08-21 18:49:41 +0300

    test: update app unit test file

--- tests/App.test.jsx
+++ tests/App.test.jsx
@@ -1,51 +1,59 @@
-import { describe, it, expect, beforeEach } from 'vitest';
+import { describe, it, expect, beforeEach, vi } from 'vitest';
 import App from '../src/app/App';
-import { fireEvent, render, screen, within } from './test-util';
+import { fireEvent, render, screen, waitFor } from './test-util';
 import userEvent from '@testing-library/user-event';
 
+vi.mock('../src/services/store/db.js');
+
 describe('App', () => {
-  beforeEach(() => {
+  it('shows start menu dialog at first with loader', async () => {
     render(<App />);
-  });
-  it('shows game start menu dialog on load', async () => {
     expect(await screen.findByRole('dialog')).toBeInTheDocument();
+    expect(screen.getByTestId('loading-message')).toBeInTheDocument();
   });
 
-  it('game start menu dialog contains username input', async () => {
-    const dialog = await screen.findByRole('dialog');
-    expect(
-      within(dialog).getByRole('textbox', { name: /name/i }),
-    ).toBeInTheDocument();
-  });
-
-  it('shows input error when name is empty and game is started', async () => {
-    const usernameInput = screen.getByRole('textbox', {
-      name: /name/i,
+  describe('On loader disappear', () => {
+    beforeEach(async () => {
+      render(<App></App>);
+      await waitFor(() =>
+        expect(screen.queryByTestId('loading-message')).not.toBeInTheDocument(),
+      );
+      expect(screen.getByText(/start/i)).toBeInTheDocument();
     });
 
-    fireEvent.change(usernameInput, { target: { value: '' } });
-    fireEvent.blur(usernameInput);
-
-    expect(usernameInput).toHaveValue('');
-
-    const startButton = screen.getByRole('button', { name: /start/i });
-    fireEvent.click(startButton);
-    expect(usernameInput).toBeInvalid();
-  });
-
-  it('does not allow names longer than 25 characters', async () => {
-    const longName =
-      'Taumatawhakatangihangakoauauotamateaturipukakapikimaungahoronukupokaiwhenuakitanatahu';
-    const usernameInput = screen.getByRole('textbox', {
-      name: /name/i,
+    it('game start menu dialog contains username input', async () => {
+      expect(screen.getByRole('textbox')).toBeInTheDocument();
     });
 
-    const user = userEvent.setup();
+    it('shows input error when name is empty and game is started', async () => {
+      const usernameInput = screen.getByRole('textbox', {
+        name: /name/i,
+      });
 
-    await user.clear(usernameInput);
-    await user.type(usernameInput, longName);
+      fireEvent.change(usernameInput, { target: { value: '' } });
+      fireEvent.blur(usernameInput);
 
-    expect(usernameInput).toHaveValue(longName.substring(0, 25));
-    expect(usernameInput).not.toBeInvalid();
+      expect(usernameInput).toHaveValue('');
+
+      const startButton = screen.getByRole('button', { name: /start/i });
+      fireEvent.click(startButton);
+      expect(usernameInput).toBeInvalid();
+    });
+
+    it('does not allow names longer than 25 characters', async () => {
+      const longName =
+        'Taumatawhakatangihangakoauauotamateaturipukakapikimaungahoronukupokaiwhenuakitanatahu';
+      const usernameInput = screen.getByRole('textbox', {
+        name: /name/i,
+      });
+
+      const user = userEvent.setup();
+
+      await user.clear(usernameInput);
+      await user.type(usernameInput, longName);
+
+      expect(usernameInput).toHaveValue(longName.substring(0, 25));
+      expect(usernameInput).not.toBeInvalid();
+    });
   });
 });
