1762a40e0f9d0f2c6f0a565119c8b4c028b94a59
Daniel Velkov <danielvelkov13@gmail.com>
2026-08-24 18:02:43 +0300

    test: add game tests for context menu open

---
+++ tests/Game.test.jsx
@@ -0,0 +1,49 @@
+import { describe, it, expect, beforeEach, vi } from 'vitest';
+import Game from '../src/components/game/Game';
+import { act, fireEvent, render, screen, within } from './test-util';
+// import userEvent from '@testing-library/user-event';
+import { MOCK_thoughts } from '../src/services/store/__mocks__/db';
+
+describe('Game', () => {
+  it('starts a 5 minute timer on render', async () => {
+    render(<Game thoughts={MOCK_thoughts} />);
+    expect(screen.getByTestId('game-timer')).toHaveTextContent(/5:00/);
+  });
+
+  it('calls onGameEnd() cb when 5 min timer runs out', async () => {
+    vi.useFakeTimers();
+    const mockHandler = vi.fn();
+    render(<Game thoughts={MOCK_thoughts} onTimerRunOut={mockHandler} />);
+    expect(mockHandler).not.toHaveBeenCalled();
+    act(() => {
+      vi.advanceTimersByTime(1000 * 60 * 5);
+    });
+    expect(mockHandler).toHaveBeenCalled();
+  });
+
+  it('displays set thoughts', async () => {
+    render(<Game thoughts={MOCK_thoughts} />);
+    for (const thought of MOCK_thoughts)
+      expect(screen.getByText(thought.name)).toBeInTheDocument();
+  });
+
+  describe('Context menu', () => {
+    beforeEach(async () => {
+      render(<Game thoughts={MOCK_thoughts} />);
+      const gameArea = screen.getByRole('img', { name: /search area/i });
+      expect(screen.queryByTestId('guess-menu')).not.toBeInTheDocument();
+      fireEvent.click(gameArea);
+    });
+
+    it('opens context menu when clicking within game screen', async () => {
+      expect(screen.getByRole('menu')).toBeInTheDocument();
+    });
+    it('context menu has each thought as option', async () => {
+      const menu = screen.getByRole('menu');
+      const menuItems = within(menu).getAllByRole('menuitem');
+      let i = 0;
+      for (const thought of MOCK_thoughts)
+        expect(menuItems[i++]).toHaveTextContent(thought.name);
+    });
+  });
+});
