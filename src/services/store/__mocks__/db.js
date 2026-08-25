13e770a423fe86b09ba347b7fbda37377a07b720
Daniel Velkov <danielvelkov13@gmail.com>
2026-08-25 18:36:06 +0300

    feat: add selected thoughts coordinates and size

--- src/services/store/__mocks__/db.js
+++ src/services/store/__mocks__/db.js
@@ -3,9 +3,33 @@ import HomoUnderground from '../../../assets/images/homosexual_underground.webp'
 import VolShitCompressor from '../../../assets/images/volumetric_shit_compressor.webp';
 
 export const MOCK_thoughts = [
-  { id: 1, name: 'Bankruptcy Sequence', image: BankSeq },
-  { id: 2, name: 'Homosexual Underground', image: HomoUnderground },
-  { id: 3, name: 'Volumetric Shit Compressor', image: VolShitCompressor },
+  {
+    id: 1,
+    name: 'Bankruptcy Sequence',
+    image: BankSeq,
+    x: 2335,
+    y: 2325,
+    width: 211,
+    height: 135,
+  },
+  {
+    id: 2,
+    name: 'Homosexual Underground',
+    image: HomoUnderground,
+    x: 83,
+    y: 1274,
+    width: 262,
+    height: 226,
+  },
+  {
+    id: 3,
+    name: 'Volumetric Shit Compressor',
+    image: VolShitCompressor,
+    x: 3570,
+    y: 1480,
+    width: 117,
+    height: 228,
+  },
 ];
 
 export async function getLeaderboard() {}
