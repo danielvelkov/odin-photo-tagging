f006b13ea509de5cacbf88343ff5cf92ea74508a
Daniel Velkov <danielvelkov13@gmail.com>
2026-08-26 22:59:41 +0300

    test: add mock methods related to db

--- src/services/store/__mocks__/db.js
+++ src/services/store/__mocks__/db.js
@@ -14,6 +14,7 @@ import MagBasedLife from '../../../assets/images/magnesium_based_lifeform.webp';
 import FeministAgenda from '../../../assets/images/inexplicable_feminist_agenda.webp';
 import IndirectTaxation from '../../../assets/images/indirect_modes_of_taxation.webp';
 import HardcoreAesthetic from '../../../assets/images/hardcore_aesthetic.webp';
+import { formatDurationBetweenDates } from '../../helpers';
 
 export const MOCK_thoughts = [
   {
@@ -194,7 +195,51 @@ export const MOCK_thoughts = [
   },
 ];
 
-export async function getLeaderboard() {}
+export let MOCK_scores = [];
+
+const addFakeScore = (score) => {
+  const mockScore = {
+    id: score.id ? score.id : Math.max(MOCK_scores.map((s) => s.id)) + 1,
+    ...score,
+  };
+  MOCK_scores.push(mockScore);
+  return mockScore;
+};
+
+export async function startGameSession(name) {
+  return new Promise((resolve) => {
+    const mockScore = addFakeScore({
+      name,
+      startTime: new Date().toString(),
+    });
+    resolve(mockScore);
+  });
+}
+
+export async function endGameSession(id) {
+  return new Promise((resolve, reject) => {
+    const mockScore = MOCK_scores.find((s) => s.id === id);
+    if (mockScore) {
+      const updatedScore = { ...mockScore, endTime: new Date().toString() };
+      MOCK_scores = [
+        ...MOCK_scores.filter((s) => s.id !== mockScore.id),
+        updatedScore,
+      ];
+      resolve(updatedScore);
+    } else reject('Missing score');
+  });
+}
+
+export async function getLeaderboard() {
+  return new Promise((resolve) =>
+    resolve(
+      MOCK_scores.map((score) => ({
+        name: score.name,
+        time: formatDurationBetweenDates(score.startTime, score.endTime),
+      })),
+    ),
+  );
+}
 
 export async function getThoughts() {
   return new Promise((resolve) => resolve(MOCK_thoughts));
