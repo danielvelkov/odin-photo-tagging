8e995a11fd2689db6ac1c2e63ce3c73479935c82
Daniel Velkov <danielvelkov13@gmail.com>
2026-09-03 13:08:17 +0300

    feat: add config file for firebase store configuration

---
+++ src/services/store/config.js
@@ -0,0 +1,8 @@
+export const config = {
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
+};
